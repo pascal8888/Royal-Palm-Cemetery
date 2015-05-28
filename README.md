fe061dbadc0c2f0c24f7a0e2560981efd8fe064f
James Schacht <pascal888@yahoo.com>
2015-05-28 09:55:25 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Royal-Palm-Cemetary
 A project to clean and analyze raw data collected about interments at Royal Palm Cemetary 101 55th Street South, Saint Petersburg Florida, 33707
+
+### From the source data:
+ROYAL PALM CEMETERY, St. Petersburg, Pinellas County, Florida
+
+File contributed for use in USGenWeb Archives by Lesleigh Laite Butts, 
+(http://www.genrecords.net/emailregistry/vols/00027.html#0006568). 
+
+USGENWEB ARCHIVES NOTICE:  These electronic pages may NOT be reproduced in any format for 
+profit or publication by any other organization or persons.  Persons or organizations 
+desiring to use this material, must obtain the written consent of the contributor, or the 
+legal representative of the submitter, and contact the listed USGenWeb archivist with proof 
+of this consent. The submitter has given permission to the USGenWeb Archives to store the 
+file permanently for free access.
+
+Copyright.  All rights reserved.
+http://www.usgwarchives.net/copyright.htm
+
+#####******************************************************************************************
+
+Notes:  Royal Palm Cemetery is located at 101 - 55th Street S., St. 
+Petersburg, Florida. Reginald H. Sumner established Royal Palm in 1921 
+after purchasing 80 acres of land between Fifty-fifth and Forty-eighth 
+streets and First and Ninth avenues south from Mr. George S. Gandy. A 
+palm lined road leads to the cemetery offices in the middle of the tract. 
+This building had once served as a chapel. Of the original parcel, 55 acres 
+remain as cemetery today. There is more than a mile of winding roads that 
+section off the cemetery.  
+
+The Pinellas Genealogy Society volunteer canvassers began canvassing Royal 
+Palm on October 14, 2000, completing the project on March 10, 2001. The following 
+individuals participated in the survey of Royal Palm Cemetery: Trish Burch, 
+Lesleigh Butts (Project Chair), Ann Cantlin, Charlie & Pat Grandmaison, Pat 
+Johnston, Kerry and JoAnn McElhaney, Rae Rose and Beth Shields. The following 
+individuals assisted with foreign language translation: Mrs. Alice Groman (Russian), 
+Mr. Stanley Shalit (Jewish), Mrs. Nancy Keizer (Dutch). The survey resulted in 
+22,069 recorded interments. Although every attempt was made to record every burial, 
+the data will not reflect those buried without headstones or headstones that have 
+gone below ground. PGS member Joseph C. Grandmaison completed the bulk of data 
+entry on this project, his wife Pat assisting with the verification process.   
+Boy Scout, Kyle Cannon, of Scout Troop 84 - West Central Florida Council supervised 
+a team of volunteers who worked on finishing the final twenty five percent of this 
+project to earn Kyle the rank of Eagle Scout. His team of volunteers were:  Boyd & 
+Cheryl Belnap, Bruce & Phyllis Belnap, Jean Boyer, Earl Brigance, Kari & Jordan Cannon, 
+Jeffrey & Kevin Cluff, Don Conrad, Larry & Diana Dean, Gene & Margie Fallon, Diane 
+Griffin, Phil & Virginia Huchton, Barbara Knibbs Peter Luginbuehl, Bob McManaway, Bob 
+Smith, Cesar Tolentino, Todd Williams.
+
+This index is provided by the Pinellas Genealogy Society.  For availability of 
+complete transcription and to view a cemetery photograph, visit the Pinellas 
+Genealogy Society at http://www.rootsweb.com/~flpgs.
--- RoyalPalmAnalysis.R
+++ RoyalPalmAnalysis.R
@@ -1,6 +1,6 @@
 ####### NOTICE - THIS DATA MAY NOT BE USED COMMERCIALLY #######
 ## [Insert full data acceptable use information here. See RPC.txt for current statement]
-## Analayze data from Royal Palm Cemetary - 101 55th Street South, Saint Petersburg, Florida, 33707. Captured manually 2000 - 2001. See raw source file (RPC.txt) for a complete description of the data collection process including persons involved.
+## Analayze data from Royal Palm cemetery - 101 55th Street South, Saint Petersburg, Florida, 33707. Captured manually 2000 - 2001. See raw source file (RPC.txt) for a complete description of the data collection process including persons involved.
 ## Load needed packages
 library(dplyr)
 library(tidyr)
@@ -10,7 +10,7 @@ library(shiny)
 if (!file.exists("RPC.txt")) {
     download.file("http://files.usgwarchives.net/fl/pinellas/cemetery/royal.txt","RPC.txt")
 }
-## Read the file into a data table starting at the first line with interrment data (51).
+## Read the file into a data table starting at the first line with interment data (51).
 rpc <- read.csv("RPC.txt",skip=50,sep=",",fill=T,header=F,col.names=c("Last_Name","First_Name_MI","Suffix","Suffix2","Honorific","Born_Died"),colClasses="character")
 ## DATA ANALYSIS - EXPERIMENTAL DESIGN (original pull 5/26/2015)
 ## Of the source data, most observations(rows) have 3 variables(columns) - Last_Name, First_Name_MI, Born_Died (actually 2 variables), but some have an additional 1 or 2 columns that store an honorific or suffix.  This analysis script will clean the source data to produce a final data set of 22,079 observations of 7 variables.  The variables are: Last_Name | First_Name_MI | Suffix | Suffix2 | Honorific | Born | Died
@@ -36,10 +36,10 @@ rpc <- rpc %>% mutate(Life_Span=ifelse(Died>1700,Died-Born,NA))
 ## Data Cleaning Complete
 bornYears <- unique(rpc$Born)  ## Find which years interred persons where born
 l <- which(bornYears > 1700,arr.ind=T)  ## Setting to + 1700 is high enough to get the oldest dates but also culls NAs and those rows where some date information is missing (e.g. 3 digits entered for year instead of 4)
-bornYears <- sort(bornYears[l]) ## This is the array for the years in which persons interred at Royal Palm Cemetary were born
+bornYears <- sort(bornYears[l]) ## This is the array for the years in which persons interred at Royal Palm cemetery were born
 diedYears <- unique(rpc$Died)  ## Find which years interred persons died
 m <- which(diedYears > 1700,arr.ind=T) ## See note above for bornYears - applies here
-diedYears <- sort(diedYears[m]) ## This is the array for the years in which persons interred at Royal Palm Cemetary died
+diedYears <- sort(diedYears[m]) ## This is the array for the years in which persons interred at Royal Palm cemetery died
 ## Figure out how many people were born each year (result == bornPerYear)
 s <- as.vector(c())
 for (i in 1:length(bornYears)) {
@@ -74,7 +74,7 @@ countSirNames[,2] <- as.numeric(countSirNames[,2]) ## set the counts to numeric 
 rm("s","s2","s3","i","m","l")
 ## Give some information about what just happened and options now
 print(
-    "Data reading, formatting, and analysis have been completed. See insert URL for a visual display of the output data. Variables left in R environment: 1) rpc - this the clean and tidy data set for Royal Palm Cemetary [data frame] 2) avgLifeSpan - this is the mean of all observations where we have both born and died data [numeric value] 3) bornYears - these are the years in which persons interred at RPC were born [numeric vector] 4) diedYears - these are the years in which persons interred at RPC died [numeric vector] 5) bornPerYear - this is number of persons interred at RPC born per each year [matrix] 6) diedPerYear - this is count of persons interred at RPC died per each year [matrix] 7) sirNames - this is a list of unique sirnames found amongst persons interred at RPC [character vector] 8) countSirNames - this is count of persons interred at RPC per sir name [data frame]"
+    "Data reading, formatting, and analysis have been completed. See insert URL for a visual display of the output data. Variables left in R environment: 1) rpc - this the clean and tidy data set for Royal Palm cemetery [data frame] 2) avgLifeSpan - this is the mean of all observations where we have both born and died data [numeric value] 3) bornYears - these are the years in which persons interred at RPC were born [numeric vector] 4) diedYears - these are the years in which persons interred at RPC died [numeric vector] 5) bornPerYear - this is number of persons interred at RPC born per each year [matrix] 6) diedPerYear - this is count of persons interred at RPC died per each year [matrix] 7) sirNames - this is a list of unique sirnames found amongst persons interred at RPC [character vector] 8) countSirNames - this is count of persons interred at RPC per sir name [data frame]"
 )
 ## Final cleanup
 ## rm("rpc","avgLifeSpan","bornYears","diedYears","bornPerYear","diedPerYear","sirNames","countSirNames")
